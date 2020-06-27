993009a70494907ac54916d552f86373326ca285
grelad <gerchenenko@gmail.com>
2020-06-27 23:33:45 +0300

    Updates

---
+++ precache-manifest.2fdbfb504491b1b6f7da0e8524321d70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe237fc0ed5bc05d43245e196f43d551",
+    "url": "/robots-rolodex-react/index.html"
+  },
+  {
+    "revision": "ed955e864bd093e2f78c",
+    "url": "/robots-rolodex-react/static/css/main.68ce2564.chunk.css"
+  },
+  {
+    "revision": "cc2cd36c0f30e51a85e4",
+    "url": "/robots-rolodex-react/static/js/2.b8aaa801.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex-react/static/js/2.b8aaa801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed955e864bd093e2f78c",
+    "url": "/robots-rolodex-react/static/js/main.73ab0a82.chunk.js"
+  },
+  {
+    "revision": "550ed78ea13145760d7c",
+    "url": "/robots-rolodex-react/static/js/runtime-main.7a230eaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robots-rolodex-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
